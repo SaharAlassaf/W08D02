d0b1ab619bc81be634c706a497905a3e56727cd9
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-11-29 20:20:14 +0300

    userSchema

--- db/index.js
+++ db/index.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const DB = process.env.DB;
-
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-mongoose.connect(`${DB}`, options, () => {
+mongoose.connect(`${process.env.DB}`, options, () => {
   try {
     console.log("DB READY TO USE");
   } catch (error) {
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server on ${PORT}`);
