002ac25a54055b126e183eb09ebf4c9ca70df187
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-11-29 20:22:57 +0300

    roleSchema

--- db/models/role.js
+++ db/models/role.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const roleSchema = new mongoose.Schema({
   role: { type: String, required: true },
-  permission: { type: Array, required: true}
+  permission: { type: Array, required: true },
 });
 
 module.exports = mongoose.model("Role", roleSchema);
