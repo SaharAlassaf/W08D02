93e174ec9e78ee988d58bc1c0a6dc180c13e8a20
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-11-29 20:40:46 +0300

    create role

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
-require("./db")
+require("./db");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
-
+const roleRouter = require("./routers/routes/role");
+app.use(roleRouter);
 
 
 const PORT = process.env.PORT || 5000;
